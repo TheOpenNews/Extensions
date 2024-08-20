2a43b62e5b8085b8e229c01c6610756102bc0fe5
t-88 <temptaki5002@gmail.com>
2024-08-20 14:03:11 +0100

    Add .

--- compile_run.sh
+++ compile_run.sh
@@ -4,16 +4,15 @@ set -eux
 
 classpath="libs/json.jar:libs/okhttp-4.9.1.jar:libs/okio-3.6.0-all.jar:libs/okio-jvm-3.4.0.jar:libs/commons-text-1.12.0.jar:libs/commons-lang3-3.15.0.jar:libs/jsoup-1.15.3.jar"
 
-
-# gkotlinc -classpath $classpath  -d  build/build.jar anynews/src/shared/*.kt anynews/trtarabi/src/extension/*.kt
-# gkotlin -classpath $classpath:build/build.jar anynews.extension.trtarabi.TrtArabiKt
+kotlinc -classpath $classpath  -d  build/build.jar anynews/src/shared/*.kt anynews/alarabiya/src/extension/*.kt
+kotlin -classpath $classpath:build/build.jar anynews.extension.alarabiya.AlarabiyaKt
 
 
 # kotlinc  -jvm-target 1.8  -language-version 1.7 -d libs/ExtensionsAbstract.jar anynews/src/shared/*.kt 
 
 
-rm apks/*.apk
-mv anynews/alarabiya/build/outputs/apk/debug/onews-alarabiya-debug.apk apks/onews-alarabiya-debug.apk
-mv anynews/aljazeera/build/outputs/apk/debug/onews-aljazeera-debug.apk apks/onews-aljazeera-debug.apk
-mv anynews/s2jnews/build/outputs/apk/debug/onews-s2jnews-debug.apk apks/onews-s2jnews-debug.apk
-mv anynews/trtarabi/build/outputs/apk/debug/onews-trtarabi-debug.apk apks/onews-trtarabi-debug.apk
+# rm apks/*.apk
+# mv anynews/alarabiya/build/outputs/apk/debug/onews-alarabiya-debug.apk apks/onews-alarabiya-debug.apk
+# mv anynews/aljazeera/build/outputs/apk/debug/onews-aljazeera-debug.apk apks/onews-aljazeera-debug.apk
+# mv anynews/s2jnews/build/outputs/apk/debug/onews-s2jnews-debug.apk apks/onews-s2jnews-debug.apk
+# mv anynews/trtarabi/build/outputs/apk/debug/onews-trtarabi-debug.apk apks/onews-trtarabi-debug.apk
